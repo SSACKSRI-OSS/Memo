b8b269f1d841cb1c46e2359a5e4603bef9632113
s-jh0756 <84397445+s-jh0756@users.noreply.github.com>
2021-05-24 05:43:08 +0900

    Add files via upload

---
+++ src/main/java/com/ssacksri/memo/MainActivity.java
@@ -0,0 +1,83 @@
+package com.ssacksri.memo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    public static SQLiteHelper sqLiteHelper;
+    ImageView add_btn;
+    ListView diary_list;
+    ArrayList<MemoModel> list;
+    MemoListAdapter adapter ;
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        list = new ArrayList<>();
+        adapter = new MemoListAdapter(this,R.layout.memo_item,list);
+        diary_list.setAdapter(adapter);
+        updateDiaryList();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sqLiteHelper = new SQLiteHelper(this,"DIARY.sqlite",null,1);
+        sqLiteHelper.queryData("CREATE TABLE IF NOT EXISTS DIARY(num INTEGER PRIMARY KEY AUTOINCREMENT , date VARCHAR, comment VARCHAR ,image BLOB)");
+
+
+
+        diary_list =(ListView)findViewById(R.id.diary_list);
+        list = new ArrayList<>();
+        adapter = new MemoListAdapter(this,R.layout.memo_item,list);
+        diary_list.setAdapter(adapter);
+        diary_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent intent = new Intent(MainActivity.this, MemoUpdateActivity.class);
+                intent.putExtra("num",list.get(position).getNum());
+                startActivity(intent);
+            }
+        });
+
+
+
+        add_btn = findViewById(R.id.add_btn);
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MemoAddActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+    private void updateDiaryList() {
+        Cursor cursor = sqLiteHelper.getData("SELECT * FROM DIARY ORDER BY date");
+        list.clear();
+        while (cursor.moveToNext()){
+
+            String num = cursor.getString(0);
+            String date = cursor.getString(1);
+            String comment = cursor.getString(2);
+            byte[] image = cursor.getBlob(3);
+
+            list.add(new MemoModel(num,date,comment,image));
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+}
---
+++ src/main/java/com/ssacksri/memo/MemoUpdateActivity.java
@@ -0,0 +1,173 @@
+package com.ssacksri.memo;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.ByteArrayOutputStream;
+
+public class MemoUpdateActivity extends AppCompatActivity {
+
+    TextView date_text;
+    EditText  comment_text;
+    ImageView add_btn,delete_btn,image_text;
+    String num;
+    public static SQLiteHelper sqLiteHelper;
+    AlertDialog dialog;
+    Uri imageUri;
+    private final int GET_GALLERY_IMAGE = 200;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo_update);
+
+        sqLiteHelper = new SQLiteHelper(this,"DIARY.sqlite",null,1);
+
+        Intent intent = getIntent();
+        num = intent.getStringExtra("num");
+
+
+        date_text= findViewById(R.id.date_text);
+        comment_text= findViewById(R.id.comment_text);
+        image_text = findViewById(R.id.image_text);
+
+        Cursor cursor = sqLiteHelper.getData("SELECT date,comment,image FROM DIARY WHERE num= '" +num+"'  ");
+        while (cursor.moveToNext()){
+            String date = cursor.getString(0);
+            date_text.setText(date);
+            String comment = cursor.getString(1);
+            comment_text.setText(comment);
+            byte[] Image = cursor.getBlob(2);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(Image,0,Image.length);
+                image_text.setImageBitmap(bitmap);
+
+
+        }
+        delete_btn = findViewById(R.id.delete_btn);
+        delete_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogDelete(num);
+            }
+        });
+
+        image_text.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(
+                        MemoUpdateActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        GET_GALLERY_IMAGE
+                );
+            }
+        });
+        add_btn= findViewById(R.id.add_btn);
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String comment = comment_text.getText().toString();
+                if (comment.equals("") ) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MemoUpdateActivity.this);
+                    dialog = builder.setMessage("내용을 입력해주세요.")
+                            .setNegativeButton("확인", null)
+                            .create();
+                    dialog.show();
+                    return;
+
+                }
+                try {
+                    sqLiteHelper.updateDiary(
+                            comment,
+                            imageViewToByte(image_text),
+                            num
+                    );
+                    Toast.makeText(getApplicationContext(),"수정했습니다",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+
+    }
+
+    private void showDialogDelete(final String num){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(MemoUpdateActivity.this);
+        dialog.setTitle("삭제");
+        dialog.setMessage("선택한 메모를 삭제하시겠습니까?");
+        dialog.setPositiveButton("네", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                try {
+                    sqLiteHelper.deleteDiary(num);
+                    Toast.makeText(getApplicationContext(),"삭제되었습니다",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                catch (Exception e){
+                    Log.e("error", e.getMessage());
+                }
+            }
+        });
+        dialog.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    public static byte[] imageViewToByte(ImageView imageView){
+        Bitmap bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == GET_GALLERY_IMAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Select Image"), GET_GALLERY_IMAGE);
+            } else {
+                Toast.makeText(getApplicationContext(), "갤러리 권한을 허용해주세요", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK ) {
+            imageUri = data.getData();
+            image_text.setImageURI(imageUri);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
